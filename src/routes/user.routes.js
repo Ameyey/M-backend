668da6c6ef82d74e8e44bec88e528da875aa00ef
ameybahugune <asd>
2025-04-20 16:31:43 +0530

    add a Rounter using

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import  { loginUser, logotUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser }  from "../controllers/user.controller.js"
+import  { loginUser, logotUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updataAccountDetails, updataUserAvatar, updataUserCoverImage }  from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -28,6 +28,13 @@ router.route("/login").post(loginUser)
 //secured route 
 
 router.route("/logout").post(verifyJWT , logotUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password")/post(verifyJWT , changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updataAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updataUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updataUserCoverImage)
 
 
 
